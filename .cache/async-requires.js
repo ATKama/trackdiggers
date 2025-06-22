85b170ecc7d170f3c2af0339de2cd282f6b83056
Lorenzo Cuadrado <LorenzoCuadrado@air-de-oceane.home>
2025-06-22 19:28:53 +0200

    Fix: remove QPAGES logic for clean build

---
+++ .cache/async-requires.js
@@ -0,0 +1,11 @@
+// prefer default export if available
+const preferDefault = m => (m && m.default) || m
+
+exports.components = {
+  "component---src-pages-404-js": () => import("./../../../src/pages/404.js" /* webpackChunkName: "component---src-pages-404-js" */),
+  "component---src-pages-cartes-js": () => import("./../../../src/pages/cartes.js" /* webpackChunkName: "component---src-pages-cartes-js" */),
+  "component---src-pages-index-js": () => import("./../../../src/pages/index.js" /* webpackChunkName: "component---src-pages-index-js" */),
+  "component---src-pages-informations-legales-js": () => import("./../../../src/pages/informations-legales.js" /* webpackChunkName: "component---src-pages-informations-legales-js" */),
+  "component---src-pages-votes-js": () => import("./../../../src/pages/votes.js" /* webpackChunkName: "component---src-pages-votes-js" */)
+}
+
---
+++ .cache/sync-requires.js
@@ -0,0 +1,13 @@
+
+// prefer default export if available
+const preferDefault = m => (m && m.default) || m
+
+
+exports.components = {
+  "component---src-pages-404-js": preferDefault(require("/Users/LorenzoCuadrado/Desktop/TRACKDIGGERS/export TD/src/pages/404.js")),
+  "component---src-pages-cartes-js": preferDefault(require("/Users/LorenzoCuadrado/Desktop/TRACKDIGGERS/export TD/src/pages/cartes.js")),
+  "component---src-pages-index-js": preferDefault(require("/Users/LorenzoCuadrado/Desktop/TRACKDIGGERS/export TD/src/pages/index.js")),
+  "component---src-pages-informations-legales-js": preferDefault(require("/Users/LorenzoCuadrado/Desktop/TRACKDIGGERS/export TD/src/pages/informations-legales.js")),
+  "component---src-pages-votes-js": preferDefault(require("/Users/LorenzoCuadrado/Desktop/TRACKDIGGERS/export TD/src/pages/votes.js"))
+}
+
