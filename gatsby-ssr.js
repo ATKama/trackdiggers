78713c69d703bb06981d23c3f3a43556ed522ac8
Lorenzo Cuadrado <LorenzoCuadrado@air-de-oceane.home>
2025-06-26 23:50:41 +0200

    enleve tarte

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,6 +1,8 @@
 const React = require("react");
 const QAPI = require("./qapi").default;
 
+const ENABLE_TARTEAUCITRON = false;
+
 exports.onRenderBody = ({ setHeadComponents, setPostBodyComponents }) => {
   setHeadComponents([
     <link
@@ -18,7 +20,7 @@ exports.onRenderBody = ({ setHeadComponents, setPostBodyComponents }) => {
       rel="stylesheet"
       href="/tarteaucitron/tarteaucitron.css"
     />,
-      <style
+    <style
       key="global-font-style"
       dangerouslySetInnerHTML={{
         __html: `
@@ -30,11 +32,13 @@ exports.onRenderBody = ({ setHeadComponents, setPostBodyComponents }) => {
     />,
   ]);
 
-  setPostBodyComponents([
+  const postBodyComponents = [
+    // Bloc désactivé pour le moment — à réactiver en changeant display: none → block
     <div
       key="cookie-blocker"
       id="cookie-blocker"
       style={{
+        display: "none", // ⬅️ Désactivation ici
         position: "fixed",
         top: 0,
         left: 0,
@@ -53,74 +57,55 @@ exports.onRenderBody = ({ setHeadComponents, setPostBodyComponents }) => {
         __html: `window.QAPI = ${JSON.stringify(QAPI || {})}`,
       }}
     />,
+  ];
 
-    <script
-      key="tarteaucitron-js"
-      src="/tarteaucitron/tarteaucitron.min.js"
-      defer
-    />,
-    <script
-      key="tarteaucitron-services"
-      src="/tarteaucitron/tarteaucitron.services.js"
-      defer
-    />,
-    <script
-      key="tarteaucitron-lang"
-      src="/tarteaucitron/lang/tarteaucitron.fr.js"
-      defer
-    />,
+  if (ENABLE_TARTEAUCITRON) {
+    postBodyComponents.push(
+      <script key="tarteaucitron-js" src="/tarteaucitron/tarteaucitron.min.js" defer />,
+      <script key="tarteaucitron-services" src="/tarteaucitron/tarteaucitron.services.js" defer />,
+      <script key="tarteaucitron-lang" src="/tarteaucitron/lang/tarteaucitron.fr.js" defer />,
+      <script
+        key="tarteaucitron-config"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.addEventListener("load", function () {
+              tarteaucitron.init({
+                privacyUrl: "/informations-legales",
+                bodyPosition: "top",
+                hashtag: "#tarteaucitron",
+                cookieName: "trackdiggersConsent",
+                orientation: "bottom",
+                groupServices: true,
+                showDetailsOnClick: true,
+                serviceDefaultState: "wait",
+                showAlertSmall: false,
+                cookieslist: true,
+                mandatory: true,
+                mandatoryCta: true,
+                AcceptAllCta: true,
+                DenyAllCta: true,
+                adblocker: false,
+                highPrivacy: false,
+                handleBrowserDNTRequest: false,
+                removeCredit: true,
+                moreInfoLink: true,
+                useExternalCss: false,
+                useExternalJs: false,
+                googleConsentMode: true,
+              });
 
-    <script
-      key="tarteaucitron-config"
-      dangerouslySetInnerHTML={{
-        __html: `
-          window.addEventListener("load", function () {
-            tarteaucitron.init({
-              privacyUrl: "/informations-legales",
-              bodyPosition: "top",
-              hashtag: "#tarteaucitron",
-              cookieName: "trackdiggersConsent",
-              orientation: "bottom",
-              groupServices: true,
-              showDetailsOnClick: true,
-              serviceDefaultState: "wait",
-              showAlertSmall: false,
-              cookieslist: true,
-              mandatory: true,
-              mandatoryCta: true,
-              AcceptAllCta: true,
-              DenyAllCta: true,
-              adblocker: false,
-              highPrivacy: false,
-              handleBrowserDNTRequest: false,
-              removeCredit: true,
-              moreInfoLink: true,
-              useExternalCss: false,
-              useExternalJs: false,
-              googleConsentMode: true,
+              tarteaucitron.user.adsensecapub = "ca-pub-1398071244867525";
+              tarteaucitron.job = tarteaucitron.job || [];
+              tarteaucitron.job.push("adsenseauto");
+
+              tarteaucitron.user.gtagUa = "G-GGZH8XN7JV";
+              tarteaucitron.job.push("gtag");
             });
-tarteaucitron.user.adsensecapub = "ca-pub-1398071244867525"; // ton identifiant
-tarteaucitron.job = tarteaucitron.job || [];
-tarteaucitron.job.push("adsenseauto");
+          `,
+        }}
+      />
+    );
+  }
 
-            tarteaucitron.user.gtagUa = "G-GGZH8XN7JV";
-            tarteaucitron.job = tarteaucitron.job || [];
-            tarteaucitron.job.push("gtag");
-
-            const interval = setInterval(() => {
-              const cookie = document.cookie
-                .split('; ')
-                .find(row => row.startsWith('trackdiggersConsent='));
-
-              if (cookie && cookie.includes("gtag=true")) {
-                const blocker = document.getElementById("cookie-blocker");
-                if (blocker) blocker.remove();
-                clearInterval(interval);
-              }
-            }, 300);
-          });
-        `,
-      }}
-    />,
-  ]);
+  setPostBodyComponents(postBodyComponents);
 };
